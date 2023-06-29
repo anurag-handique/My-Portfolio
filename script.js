d935eb37f3550d1c11da7d6ba4d212bf02af2460
anurag-handique <anuraghandique.dev@gmail.com>
2023-06-29 23:47:14 +0530

    This is my portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+function openChatPage() {
+  const chatPage = document.getElementById("chat-page");
+  chatPage.classList.add("open");
+}
+
+function closeChatPage() {
+  const chatPage = document.getElementById("chat-page");
+  chatPage.classList.remove("open");
+}
